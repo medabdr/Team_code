c08835004741616d8af9042487ff8c24cdeed465
asma <asmadahh@gmail.com>
2025-12-03 18:53:03 +0000

    ajout du menu manquant sur header

--- TM-CODE/src/app/shared/components/header/header.component.ts
+++ TM-CODE/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Fermer si on clique en dehors
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+
+    if (!target.closest('.left')) {
+      this.menuOpen = false;
+    }
+  }
 }
